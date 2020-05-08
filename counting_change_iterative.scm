4903892de1edc0bed9b641a5454bb676b4a712d5
Miles McBain <miles.mcbain@gmail.com>
2020-05-08 21:32:45 +1000

    counting change iterative works
    1 year later... no biggie

--- counting_change_iterative.scm
+++ counting_change_iterative.scm
@@ -1,2 +1,22 @@
 #lang sicp
 
+(define (count-change x)
+  (define (count-change-itr n-5 n-10 n-25 times)
+    (cond ((>= (* n-5 5) 10) (count-change-itr (- n-5 2)
+                                               (+ n-10 1)
+                                               n-25
+                                               (+ times 1)))
+          ((>= (* n-10 10) 30) (count-change-itr (+ n-5 1 (* (- n-10 3) 2))
+                                                 0
+                                                 (+ n-25 1)
+                                                 (+ times 1)))
+          ((=  (+ (* n-5 5) (* n-10 10)) 25) (count-change-itr (+ n-5 (* (- n-10 2) 2))
+                                                               0
+                                                               (+ n-25 1)
+                                                               (+ times 1)))
+          (else times)))
+
+  (count-change-itr (/ x 5) 0 0 1))
+
+(count-change 30)
+(count-change 60)
--- counting_change_recursive.scm
+++ counting_change_recursive.scm
@@ -6,14 +6,15 @@
     (cond ((< total 0) 0)
           ((= total 0) 1)
           (else (+ times
-                   (if (>= max-coin 20)
-                       (count-combinations (- total 20) 20 times)
+                   (if (>= max-coin 25)
+                       (count-combinations (- total 25) 25 times)
                        0)
                    (if (>= max-coin 10)
                        (count-combinations (- total 10) 10 times)
                        0)
                    (count-combinations (- total 5) 5 times)))))
 
-  (count-combinations total 20 0))
+  (count-combinations total 25 0))
 
-(count-change-combinations 20)
+(count-change-combinations 30)
+(count-change-combinations 60)
