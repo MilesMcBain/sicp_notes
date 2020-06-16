2bcc90e4a32a6cb048da311cf95a52d1ccf8ed49
Miles McBain <miles.mcbain@gmail.com>
2020-06-16 21:24:45 +1000

    extended exercise

---
+++ ex2_7.scm
@@ -0,0 +1,36 @@
+#lang sicp
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound x)))
+        (p4 (* (upper-bound x) (lower-bound x))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (mul-interval
+   x
+   (make-interval (/ 1.0 (upper-bound y))
+                  (/ 1.0 (lower-bound y)))))
+
+(define (make-interval a b) (cons a b))
+
+;; ex 2.7
+(define (upper-bound int)
+  (cdr int))
+
+(define (lower-bound int)
+  (car int))
+
+;; ex 2.8
+(define (sub-interval x y)
+  (make-interval (- (upper-bound x) (lower-bound y))
+                 (- (lower-bound x) (upper-bound y))))
+
+;; ex 2.9
+;; see notes
