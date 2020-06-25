b2a1d21d0359f0eed0dc36f138e511ea155f9c47
Miles McBain <miles.mcbain@gmail.com>
2020-06-25 23:38:17 +1000

    update chap2 notes, solve 2_11

--- ex2_7.scm
+++ ex2_7.scm
@@ -4,19 +4,70 @@
   (make-interval (+ (lower-bound x) (lower-bound y))
                  (+ (upper-bound x) (upper-bound y))))
 
+
 (define (mul-interval x y)
-  (let ((p1 (* (lower-bound x) (lower-bound y)))
-        (p2 (* (lower-bound x) (upper-bound y)))
-        (p3 (* (upper-bound x) (lower-bound x)))
-        (p4 (* (upper-bound x) (lower-bound x))))
-    (make-interval (min p1 p2 p3 p4)
-                   (max p1 p2 p3 p4))))
+  (let ((pos-x-lower (positive? (lower-bound x)))
+        (pos-x-upper (positive? (upper-bound x)))
+        (pos-y-lower (positive? (lower-bound y)))
+        (pos-y-upper (positive? (upper-bound y))))
+    (cond ((and pos-x-lower pos-x-upper
+                pos-y-lower pos-y-upper)
+           ;; all pos
+           (make-interval (* (lower-bound x) (lower-bound y))
+                          (* (upper-bound x) (upper-bound y))))
+          ((and (not pos-x-lower) (not pos-x-upper)
+                (not pos-y-lower) (not pos-y-upper))
+           ;; all neg
+           (make-interval (* (upper-bound x) (upper-bound y))
+                          (* (lower-bound x) (lower-bound y))))
+          ((and (not pos-x-lower) (not pos-x-upper)
+                pos-y-lower pos-y-upper)
+           ;; x neg, y pos
+           (make-interval (* (lower-bound x) (upper-bound y))
+                          (* (upper-bound x) (lower-bound y))))
+          ((and pos-x-lower pos-x-upper
+                (not pos-y-lower) (not pos-y-upper))
+           ;; x pos, y neg
+           (make-interval (* (upper-bound x) (lower-bound y))
+                          (* (lower-bound x) (upper-bound y))))
+          ((and (not pos-x-lower) pos-x-upper
+                 pos-y-lower pos-y-upper)
+           ;; x split, y pos
+           (make-interval (* (lower-bound x) (upper-bound y))
+                          (* (upper-bound x) (upper-bound y))))
+          ((and pos-x-lower pos-x-upper
+                (not pos-y-lower) pos-y-upper)
+           ;; x pos, y split
+           (make-interval (* (upper-bound x) (lower-bound y))
+                          (* (upper-bound x) (upper-bound y))))
+          ((and (not pos-x-lower) (not pos-x-upper )
+                (not pos-y-lower ) pos-y-upper)
+           ;; x neg, y split
+           (make-interval (* (lower-bound x) (upper-bound y))
+                          (* (lower-bound x) (lower-bound y))))
+          ((and (not pos-x-lower) pos-x-upper
+                (not pos-y-lower) (not pos-y-upper))
+           ;; x split, y neg
+           (make-interval (* (upper-bound x) (lower-bound y))
+                          (* (lower-bound x) (lower-bound y))))
+          ((and (not pos-x-lower) pos-x-upper
+                (not pos-y-lower) pos-y-upper)
+           ;; x split, y split
+           (let ((p1 (* (lower-bound x) (lower-bound y)))
+                 (p2 (* (lower-bound x) (upper-bound y)))
+                 (p3 (* (upper-bound x) (lower-bound y)))
+                 (p4 (* (upper-bound x) (upper-bound y))))
+             (make-interval (min p1 p2 p3 p4)
+                            (max p1 p2 p3 p4)))))))
 
 (define (div-interval x y)
-  (mul-interval
-   x
-   (make-interval (/ 1.0 (upper-bound y))
-                  (/ 1.0 (lower-bound y)))))
+  (cond ((and (< (lower-bound y) 0) (> (upper-bound y) 0)) ;; ex 2.10
+         (newline)
+         (display "Can't divide by an interval spanning 0!"))
+        (else (mul-interval
+               x
+               (make-interval (/ 1.0 (upper-bound y))
+                              (/ 1.0 (lower-bound y)))))))
 
 (define (make-interval a b) (cons a b))
 
@@ -34,3 +85,13 @@
 
 ;; ex 2.9
 ;; see notes
+
+;; ex 2.11
+(mul-interval (make-interval -2 3)
+              (make-interval 1 4))
+
+(mul-interval (make-interval -4 -1)
+              (make-interval 1 4))
+
+(mul-interval (make-interval -4 3)
+              (make-interval -5 8))
