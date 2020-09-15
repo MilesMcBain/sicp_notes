49104c470d55428f768d4252eb2b1cc86a4a453b
Miles McBain <miles.mcbain@gmail.com>
2020-09-15 19:57:10 +1000

    up-split

--- ex2_45.scm
+++ ex2_45.scm
@@ -1,3 +1,7 @@
 #lang sicp
 
-(define )
+(define (up-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (up-split painter (- n 1))))
+        (above painter (besie smaller smaller)))))
