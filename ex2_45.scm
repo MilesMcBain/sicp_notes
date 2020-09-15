d5322dbcaf2effc0247fafa843eb60a4d9ece3cb
Miles McBain <miles.mcbain@gmail.com>
2020-09-15 20:03:35 +1000

    use sicp picture language

--- ex2_45.scm
+++ ex2_45.scm
@@ -1,7 +1,10 @@
 #lang sicp
+(#%require sicp-pict)
 
 (define (up-split painter n)
   (if (= n 0)
       painter
       (let ((smaller (up-split painter (- n 1))))
-        (above painter (besie smaller smaller)))))
+        (below (beside smaller smaller) painter))))
+
+(paint (up-split einstein 4))
