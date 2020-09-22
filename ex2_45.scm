796bb2ba496f1cb0ce173ceacc8bd1941ba7defb
Miles McBain <miles.mcbain@gmail.com>
2020-09-22 21:08:01 +1000

    splitting painters

--- ex2_45.scm
+++ ex2_45.scm
@@ -1,10 +1,20 @@
 #lang sicp
 (#%require sicp-pict)
 
-(define (up-split painter n)
-  (if (= n 0)
-      painter
-      (let ((smaller (up-split painter (- n 1))))
-        (below (beside smaller smaller) painter))))
+(define (split first-partition second-partition)
+  (define (do-split painter n)
+    (if (= n 0)
+        painter
+        (let ((smaller (do-split painter (- n 1))))
+          (first-partition painter
+                           (second-partition smaller smaller)))))
 
-(paint (up-split einstein 4))
+    (lambda (painter n)
+      (do-split painter n)))
+
+(define right-split (split beside below))
+(define up-split (split below beside))
+
+(paint (right-split einstein 2))
+
+(paint (up-split einstein 2))
