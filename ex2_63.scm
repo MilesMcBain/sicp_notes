1f87e269c4eba97f6cb12cd28593d79e5ad975bd
Miles McBain <miles.mcbain@gmail.com>
2021-02-10 21:47:25 +1000

    bits for ex 2_63

---
+++ ex2_63.scm
@@ -0,0 +1,61 @@
+#lang sicp
+
+;; tree interface
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right)
+  (list entry left right))
+
+;; tree to list helpers
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append (tree->list-1 (left-branch tree))
+              (cons (entry tree)
+                    (tree->list-1
+                     (right-branch tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list (left-branch tree)
+                     (cons (entry tree)
+                           (copy-to-list
+                            (right-branch tree)
+                            result-list)))))
+  (copy-to-list tree '()))
+
+;test trees
+(define tree1 (make-tree 7
+                         (make-tree 3
+                                    (make-tree 1 '() '())
+                                    (make-tree 5 '() '()))
+                         (make-tree 9
+                                    '()
+                                    (make-tree 11 '() '()))))
+
+(define tree2 (make-tree 3
+                          (make-tree 1 '() '())
+                          (make-tree 7
+                                     (make-tree 5 '() '())
+                                     (make-tree 9
+                                                '()
+                                                (make-tree 11 '() '())))))
+
+(define tree3 (make-tree 5
+                         (make-tree 3
+                                    (make-tree 1 '() '())
+                                    '())
+                         (make-tree 9
+                                    (make-tree 7 '() '())
+                                    (make-tree 11 '() '()))))
+
+(tree->list-1 tree1)
+(tree->list-2 tree1)
+
+(tree->list-1 tree2)
+(tree->list-2 tree2)
+(tree->list-1 tree3)
+(tree->list-2 tree3)
